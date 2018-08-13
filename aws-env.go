e7ead499c2068403ad58e477317f9f50596340e2
umetaro <umetaro@users.noreply.github.com>
2018-08-13 10:55:20 -0700

    node

--- aws-env.go
+++ aws-env.go
@@ -1,119 +1,119 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"path"
-	"strconv"
-	"strings"
+  "fmt"
+  "log"
+  "os"
+  "path"
+  "strconv"
+  "strings"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/ec2metadata"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ssm"
+  "github.com/aws/aws-sdk-go/aws"
+  "github.com/aws/aws-sdk-go/aws/ec2metadata"
+  "github.com/aws/aws-sdk-go/aws/session"
+  "github.com/aws/aws-sdk-go/service/ssm"
 )
 
 func main() {
-	if os.Getenv("AWS_ENV_PATH") == "" {
-		log.Println("aws-env running locally, without AWS_ENV_PATH")
-	} else {
-		// recursivePtr := flag.Bool("recursive", false, "recursively process parameters on path")
-		// flag.Parse()
+  if os.Getenv("AWS_ENV_PATH") == "" {
+    log.Println("aws-env running locally, without AWS_ENV_PATH")
+  } else {
+    // recursivePtr := flag.Bool("recursive", false, "recursively process parameters on path")
+    // flag.Parse()
 
-		sess := CreateSession()
-		client := CreateClient(sess)
-		recursive := false
-		if os.Getenv("AWS_ENV_RECURSIVE") != "" {
-			r, err := strconv.ParseBool(os.Getenv("AWS_ENV_RECURSIVE"))
-			if err != nil {
-				panic(err.Error() + "\nSee: https://golang.org/pkg/strconv/#ParseBool")
-			}
-			recursive = r
-		}
-		ExportVariables(client, os.Getenv("AWS_ENV_PATH"), recursive, "")
-	}
-	// fmt.Println("os.Args = ")
-	// for index, element := range os.Args {
-	// 	fmt.Println(index, "=>", element)
-	// }
-	// if len(os.Args) >= 2 {
-	// 	binary, lookErr := exec.LookPath(os.Args[1])
-	// 	if lookErr != nil {
-	// 		panic(lookErr)
-	// 	}
-	//
-	// 	env := os.Environ()
-	// 	args := os.Args[1:]
-	// 	execErr := syscall.Exec(binary, args, env)
-	// 	if execErr != nil {
-	// 		panic(execErr)
-	// 	}
-	// }
+    sess := CreateSession()
+    client := CreateClient(sess)
+    recursive := false
+    if os.Getenv("AWS_ENV_RECURSIVE") != "" {
+      r, err := strconv.ParseBool(os.Getenv("AWS_ENV_RECURSIVE"))
+      if err != nil {
+        panic(err.Error() + "\nSee: https://golang.org/pkg/strconv/#ParseBool")
+      }
+      recursive = r
+    }
+    ExportVariables(client, os.Getenv("AWS_ENV_PATH"), recursive, "")
+  }
+  // fmt.Println("os.Args = ")
+  // for index, element := range os.Args {
+  //  fmt.Println(index, "=>", element)
+  // }
+  // if len(os.Args) >= 2 {
+  //  binary, lookErr := exec.LookPath(os.Args[1])
+  //  if lookErr != nil {
+  //    panic(lookErr)
+  //  }
+  //
+  //  env := os.Environ()
+  //  args := os.Args[1:]
+  //  execErr := syscall.Exec(binary, args, env)
+  //  if execErr != nil {
+  //    panic(execErr)
+  //  }
+  // }
 
 }
 
 func CreateSession() *session.Session {
-	sess := session.Must(session.NewSession())
-	if len(aws.StringValue(sess.Config.Region)) == 0 {
-		meta := ec2metadata.New(sess)
-		identity, err := meta.GetInstanceIdentityDocument()
-		if err != nil {
-			return session.Must(nil, err)
-		}
-		return session.Must(session.NewSession(&aws.Config{
-			Region: aws.String(identity.Region),
-		}))
-	}
-	return sess
+  sess := session.Must(session.NewSession())
+  if len(aws.StringValue(sess.Config.Region)) == 0 {
+    meta := ec2metadata.New(sess)
+    identity, err := meta.GetInstanceIdentityDocument()
+    if err != nil {
+      return session.Must(nil, err)
+    }
+    return session.Must(session.NewSession(&aws.Config{
+      Region: aws.String(identity.Region),
+    }))
+  }
+  return sess
 }
 
 func CreateClient(sess *session.Session) *ssm.SSM {
-	return ssm.New(sess)
+  return ssm.New(sess)
 }
 
 func ExportVariables(client *ssm.SSM, path string, recursive bool, nextToken string) {
-	input := &ssm.GetParametersByPathInput{
-		Path:           &path,
-		WithDecryption: aws.Bool(true),
-		Recursive:      aws.Bool(recursive),
-	}
+  input := &ssm.GetParametersByPathInput{
+    Path:           &path,
+    WithDecryption: aws.Bool(true),
+    Recursive:      aws.Bool(recursive),
+  }
 
-	if nextToken != "" {
-		input.SetNextToken(nextToken)
-	}
+  if nextToken != "" {
+    input.SetNextToken(nextToken)
+  }
 
-	output, err := client.GetParametersByPath(input)
+  output, err := client.GetParametersByPath(input)
 
-	if err != nil {
-		log.Panic(err)
-	}
+  if err != nil {
+    log.Panic(err)
+  }
 
-	for _, element := range output.Parameters {
-		PrintExportParameter(path, element)
-		SetExportParameter(element)
-	}
+  for _, element := range output.Parameters {
+    PrintExportParameter(path, element)
+    SetExportParameter(element)
+  }
 
-	if output.NextToken != nil {
-		ExportVariables(client, path, recursive, *output.NextToken)
-	}
+  if output.NextToken != nil {
+    ExportVariables(client, path, recursive, *output.NextToken)
+  }
 }
 
 func PrintExportParameter(path string, parameter *ssm.Parameter) {
-	name := *parameter.Name
-	value := *parameter.Value
+  name := *parameter.Name
+  value := *parameter.Value
 
-	env := strings.Replace(strings.Trim(name[len(path):], "/"), "/", "_", -1)
-	value = strings.Replace(value, "\n", "\\n", -1)
-	value = strings.Replace(value, "'", "\\'", -1)
+  env := strings.Replace(strings.Trim(name[len(path):], "/"), "/", "_", -1)
+  value = strings.Replace(value, "\n", "\\n", -1)
+  value = strings.Replace(value, "'", "\\'", -1)
 
-	fmt.Printf("export %s=%s\n", env, value)
+  fmt.Printf("%s=%s ", env, value)
 }
 
 func SetExportParameter(parameter *ssm.Parameter) {
-	name := *parameter.Name
-	value := *parameter.Value
+  name := *parameter.Name
+  value := *parameter.Value
 
-	_, envName := path.Split(name)
-	os.Setenv(envName, value)
+  _, envName := path.Split(name)
+  os.Setenv(envName, value)
 }
